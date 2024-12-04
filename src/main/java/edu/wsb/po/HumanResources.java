875e88d522f53b6f20ceed974f1ed85901062164
Arek <arkadiuszlasia@gmail.com>
2024-12-04 02:30:38 +0100

    Create HumanResources.java file

---
+++ src/main/java/edu/wsb/po/HumanResources.java
@@ -0,0 +1,37 @@
+package edu.wsb.po;
+
+import java.util.Scanner;
+
+public class HumanResources {
+    private static final Scanner scanner = new Scanner(System.in);
+    private static final DoctorManager doctorManager = new DoctorManager();
+
+    public static void main(String[] args) {
+        while (true) {
+            // Menu options
+            System.out.println("-----Doctor Manager-----");
+            System.out.println("1. Add a Doctor's profile");
+            System.out.println("2. Add a specialty to a doctor");
+            System.out.println("3. Exit");
+            System.out.println("Choose an option: ");
+
+            int choice = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (choice) {
+                case 1:
+                    doctorManager.interactiveAddDoctor();
+                    break;
+                    case 2:
+                        break;
+                        case 3:
+                            System.out.println("Exiting...");
+                            return;
+                            default:
+                                System.out.println("Invalid choice, try again." +
+                                        "");
+            }
+
+        }
+    }
+}
