57ddb59c1d8fdcad3284b5cbb112e4ea97469912
Arek <arkadiuszlasia@gmail.com>
2024-12-06 21:23:53 +0100

    Add MainInterface file

---
+++ src/main/java/edu/wsb/po/MainInterface.java
@@ -0,0 +1,44 @@
+package edu.wsb.po;
+
+import java.util.Scanner;
+
+public class MainInterface {
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        //Menu options
+        while(true){
+            System.out.println("Choose the interface to open: ");
+            System.out.println("1. Reception");
+            System.out.println("2. Human Resources");
+            System.out.println("3. Management");
+            System.out.println("4. Exit");
+
+            int choice = scanner.nextInt();
+            scanner.nextLine();
+            switch (choice) {
+                case 1:
+                    Reception receptionInterface = new Reception();
+                    receptionInterface.start();
+                    break;
+                    case 2:
+                        HumanResources humanResourcesInterface = new HumanResources();
+                        humanResourcesInterface.start();
+                        break;
+                        case 3:
+                            System.out.println("ManagementInterface, stay tuned");
+                            break;
+                            case 4:
+                                System.out.println("Exiting...");
+                                return;
+                                default:
+                                    System.out.println("Invalid choice, try again");
+
+
+
+            }
+
+
+
+    }
+}}
